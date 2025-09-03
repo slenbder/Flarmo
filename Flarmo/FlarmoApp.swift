65c0d8a7e3cd56da45a0f79cb3bb70a2cc3006b2
slenbder <marjassler@ya.ru>
2025-09-03 14:06:56 +0300

    Initial Commit

---
+++ Flarmo/FlarmoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlarmoApp.swift
+//  Flarmo
+//
+//  Created by Кирилл Марьясов on 9/3/25.
+//
+
+import SwiftUI
+
+@main
+struct FlarmoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
